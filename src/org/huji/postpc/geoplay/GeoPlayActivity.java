64b78a465ece157f7e070c7487b676cc43012e81
Michael <Michael@VaVaiVaio>
2011-09-21 01:26:36 +0300

    commit

--- src/org/huji/postpc/geoplay/GeoPlayActivity.java
+++ src/org/huji/postpc/geoplay/GeoPlayActivity.java
@@ -10,29 +10,19 @@ import android.widget.CheckBox;
 
 public class GeoPlayActivity extends Activity implements OnClickListener {
 	private Button btnStart;
-	private CheckBox displayWelcomeMsg;
-	
-	private boolean showWelcomeMsg;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(this);
     }
-    
-//    public void setHomeScreenLayout(View view) {
-//    	Intent myIntent = new Intent(view.getContext(), HomeActivity.class);
-//        startActivityForResult(myIntent, 0);
-//    }
-//    public void setPlayerLayout(View view) {
-//    	Intent myIntent = new Intent(view.getContext(), PlayerActivity.class);
-//        startActivityForResult(myIntent, 0);
-//    } 
-    
+        
     public void onClick(View v) {
-    
+    	//Start the background service
+        startService(new Intent(GeoPlayService.class.getName()));
     }
 }
--- src/org/huji/postpc/geoplay/GeoPlayService.java
+++ src/org/huji/postpc/geoplay/GeoPlayService.java
@@ -1,14 +1,50 @@
 package org.huji.postpc.geoplay;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 public class GeoPlayService extends Service {
-
+	
+	private static final String TAG = GeoPlayService.class.getSimpleName();
+	
+	private double mLongitude = 0;
+	private double mLatitude = 0;
+	
 	@Override
 	public void onCreate() {
-		//TODO: Implement
+		// Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+              // Called when a new location is found by the network location provider.
+            	mLongitude = location.getLongitude();
+            	mLatitude = location.getLatitude();
+            	Log.d("geoplay", "got location!");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+
+          };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 	}
 
 	@Override
